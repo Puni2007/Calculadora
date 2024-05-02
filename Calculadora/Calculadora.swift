0ae69824597e2938d16d8d9a599e480f8bc39531
Sonia Ujaque <sonia.ujaqueortiz@plexus.tech>
2024-05-02 11:47:07 +0200

    Creación Oulets y Actions

--- Calculadora/Calculadora.swift
+++ Calculadora/Calculadora.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class Calculadora: UIViewController {
 
+    //Label resultado
+    @IBOutlet weak var muestraResultado: UILabel!
+    
+    //Numeros
+    @IBOutlet weak var numero0: UIButton!
+    @IBOutlet weak var numero1: UIButton!
+    @IBOutlet weak var numero2: UIButton!
+    @IBOutlet weak var numero3: UIButton!
+    @IBOutlet weak var numero4: UIButton!
+    @IBOutlet weak var numero5: UIButton!
+    @IBOutlet weak var numero6: UIButton!
+    @IBOutlet weak var numero7: UIButton!
+    @IBOutlet weak var numero8: UIButton!
+    @IBOutlet weak var numero9: UIButton!
+    
+    //Operadores
+    @IBOutlet weak var operadorAC: UIButton!
+    @IBOutlet weak var operadorMasMenos: UIButton!
+    @IBOutlet weak var operadorPorcentaje: UIButton!
+    @IBOutlet weak var operadorDividir: UIButton!
+    @IBOutlet weak var operadorMultiplicar: UIButton!
+    @IBOutlet weak var operadorRestar: UIButton!
+    @IBOutlet weak var operadorSumar: UIButton!
+    @IBOutlet weak var operadorResultado: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //Acciones
+    @IBAction func operadorACAccion(_ sender: UIButton) {
+        
+    }
+    @IBAction func operadorMasMenosAccion(_ sender: UIButton) {
+        
+    }
+    @IBAction func operadorPorcentajeAccion(_ sender: UIButton) {
+        
+    }
+    @IBAction func operadorDividirAccion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operadorMultiplicarAccion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operadorRestarAccion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operadorSumarAccion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operadorResultadoAccion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func accionNumeros(_ sender: UIButton) {
+        
+        
+    }
+    
+    //Funciones
+    
 
 }
 
